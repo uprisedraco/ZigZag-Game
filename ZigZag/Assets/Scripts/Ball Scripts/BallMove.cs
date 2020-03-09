3c00280ea63bbe468142b1f2a3cee8ec6eb97582
uprisedraco <uprisedraco@gmail.com>
2020-03-09 18:19:02 +0200

    Ball move, gameplay controller

---
+++ ZigZag/Assets/Scripts/Ball Scripts/BallMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool rollLeft;
+
+    [SerializeField]
+    private float speed = 4f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rollLeft = true;
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void FixedUpdate()
+    {
+        if (GameplayController.instance.gamePlaying)
+        {
+            if (rollLeft)
+            {
+                rb.velocity = new Vector3(-speed, Physics.gravity.y, 0f);
+            }
+            else
+            {
+                rb.velocity = new Vector3(0f, Physics.gravity.y, speed);
+            }
+        }
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!GameplayController.instance.gamePlaying)
+            {
+                GameplayController.instance.gamePlaying = true;
+            }
+        }
+
+        if (GameplayController.instance.gamePlaying)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rollLeft = !rollLeft;
+            }
+        }
+    }
+}
---
+++ ZigZag/Assets/Scripts/Game Controllers/GameplayController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    public static GameplayController instance;
+
+    [HideInInspector]
+    public bool gamePlaying;
+
+    void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void OnDisable()
+    {
+        instance = null;
+    }
+
+    void MakeSingleton()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+}
